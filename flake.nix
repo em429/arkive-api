53f6bf6cd952d5b4d889bc7bb9928d3c7d61f8f4
Mate Hajzso <ajz@lambd.ax>
2022-04-25 21:38:47 +0200

    move nixosModule to nixos folder

--- flake.nix
+++ flake.nix
@@ -87,42 +87,9 @@
               # Use in your NixOS configuration:
               #   services.arkive-api.enable = true;
               #   services.arkive-api.db_path = /data/arkive.db
-              nixosModule = { lib, config, ... }:
-                let
-                  cfg = config.services.arkive-api;
-                in
-                  {
-                    options = {
-                      services.arkive-api = {
-                        enable = lib.mkEnableOption "enables arkive-api service";
-                        db_path = lib.mkOption {
-                          type = lib.types.path;
-                          example = /home/user/arkive_db.sqlite;
-                          description = "Path to sqlite database";
-                        };
-                        package = lib.mkOption {
-                          type = lib.types.package;
-                          default = self.defaultPackage;
-                        };
-                      };
-                    };
-
-                    config = lib.mkIf cfg.enable {
-                      nixpkgs.overlays = [ self.overlay ];
-                      users.users.arkive.isSystemUser = true;
-
-                      systemd.services.arkive-api = {
-                        after = [ "network.target" ];
-                        serviceConfig = {
-                          Type = "simple";
-                          User = "arkive";
-                          Group = "arkive";
-                          ExecStart = "${cfg.package}/bin/prod";
-                        };
-                      };
-                    };
-                  };
-
+              #
+              nixosModules.arkive-api = import ./nixos;
+              nixosModule = self.nixosModules.arkive-api;
 
             }
       )
