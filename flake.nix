03bad91b773caeee05555aba0b4ca4a562f6b5d3
xamogast <mate@lambd.ax>
2022-04-25 09:23:29 +0200

    nix flake: add base of nixosModule

--- flake.nix
+++ flake.nix
@@ -12,11 +12,12 @@
         poetry2nix.overlay
         (final: prev: {
           # The application
-          arkive-py = prev.poetry2nix.mkPoetryApplication {
+          arkive-api = prev.poetry2nix.mkPoetryApplication {
             projectDir = ./.;
           };
         })
       ];
+
     } // (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
@@ -26,16 +27,48 @@
       in rec
       {
         packages = {
-          package = pkgs.arkive-py;
-          # image = pkgs.arkive-py.image;
-          # tarball = pkgs.arkive-py.tarball;
+          package = pkgs.arkive-api;
+          # image = pkgs.arkive-api.image;
+          # tarball = pkgs.arkive-api.tarball;
         };
+
         # 'nix build' to build default package
         defaultPackage = packages.package;
 
         apps = {
-          arkive-py = pkgs.arkive-py;
+          arkive-api = pkgs.arkive-api;
         };
-        defaultApp = pkgs.arkive-py;
+        defaultApp = pkgs.arkive-api;
+
+        nixosModules.arkive-api = { pkgs, ...}: {
+            options = {
+                services.arkive-api = {
+                    enable = lib.mkEnableOption "enables arkive-api service";
+                    db_path = lib.mkOption {
+                        type = lib.types.path;
+                        example = /home/user/arkive_db.sqlite;
+                        description = "Path to sqlite database";
+                    };
+                    package = mkOption {
+                        type = lib.types.package;
+                        default = self.defaultPackage;
+                    };
+                };
+            };
+
+            config = lib.mkIf config.services.arkive-api.enable {
+                users.users.arkive.isSystemUser = true;
+                systemd.services.arkive-api = {
+                    after = [ "network.target" ];
+                    serviceConfig = {
+                        Type = "simple";
+                        User = "arkive";
+                        Group = "arkive";
+                        ExecStart = "${cfg.package}/bin/prod";
+                    };
+                };
+            };
+        };
+
       }));
 }
