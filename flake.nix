925a8af89ea4c98c4a285f86d175c0268be4c934
Mate Hajzso <ajz@lambd.ax>
2022-04-25 11:18:06 +0200

    add proper devshell

--- flake.nix
+++ flake.nix
@@ -4,22 +4,25 @@
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "nixpkgs/nixos-21.11";
   inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+  inputs.devshell.url = "github:numtide/devshell";
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix, devshell }:
     {
       # Nixpkgs overlay providing the application
       overlay = nixpkgs.lib.composeManyExtensions [
         poetry2nix.overlay
+        devshell.overlay
         (
           final: prev: {
             # The application
             arkive-api = prev.poetry2nix.mkPoetryApplication {
               projectDir = ./.;
             };
+
           }
         )
       ];
-
     } // (
       flake-utils.lib.eachDefaultSystem (
         system:
@@ -28,52 +31,94 @@
               inherit system;
               overlays = [ self.overlay ];
             };
+
+            ## Docker image
+            #    nix build '.#image'
+            #    The resulting image can be loaded directly:
+            #      docker load < result
+            #    And executed:
+            #      docker run -d --rm -p 3042:3042 arkive-api
+            image = pkgs.dockerTools.buildLayeredImage {
+              name = "arkive-api";
+              tag = "latest";
+              # The closure of config is automatically included in the closure of
+              # the final image, so no need to explicitly set contents property.
+              config = {
+                Cmd = [
+                  "${pkgs.arkive-api}/bin/prod"
+                ];
+                ExposedPorts = { "3042/tcp" = {}; };
+              };
+            };
+
+            nixosModule = { lib, config, ... }:
+              let
+                cfg = config.services.arkive-api;
+              in
+                {
+                  options = {
+                    services.arkive-api = {
+                      enable = lib.mkEnableOption "enables arkive-api service";
+                      db_path = lib.mkOption {
+                        type = lib.types.path;
+                        example = /home/user/arkive_db.sqlite;
+                        description = "Path to sqlite database";
+                      };
+                      package = lib.mkOption {
+                        type = lib.types.package;
+                        default = self.defaultPackage;
+                      };
+                    };
+                  };
+
+                  config = lib.mkIf cfg.enable {
+                    nixpkgs.overlays = [ self.overlay ];
+                    users.users.arkive.isSystemUser = true;
+
+                    systemd.services.arkive-api = {
+                      after = [ "network.target" ];
+                      serviceConfig = {
+                        Type = "simple";
+                        User = "arkive";
+                        Group = "arkive";
+                        ExecStart = "${cfg.package}/bin/prod";
+                      };
+                    };
+                  };
+                };
           in
             rec
             {
               packages = {
-                package = pkgs.arkive-api;
-                # image = pkgs.arkive-api.image;
-                # tarball = pkgs.arkive-api.tarball;
+                arkive-api-package = pkgs.arkive-api;
+                # arkive-api-image = image;
+                # arkive-api-tarball = tarball;
               };
-
               # 'nix build' to build default package
-              defaultPackage = packages.package;
+              defaultPackage = packages.arkive-api-package;
 
-              apps = {
-                arkive-api = pkgs.arkive-api;
-              };
-              defaultApp = pkgs.arkive-api;
+              nixosModules = { arkive-api = nixosModule; };
 
-              nixosModules.arkive-api = { pkgs, ... }: {
-                options = {
-                  services.arkive-api = {
-                    enable = lib.mkEnableOption "enables arkive-api service";
-                    db_path = lib.mkOption {
-                      type = lib.types.path;
-                      example = /home/user/arkive_db.sqlite;
-                      description = "Path to sqlite database";
-                    };
-                    package = mkOption {
-                      type = lib.types.package;
-                      default = self.defaultPackage;
+              devShell =
+                let
+                  pyEnv = pkgs.poetry2nix.mkPoetryEnv {
+                    projectDir = ./.;
+                    editablePackageSources = {
+                      arkive-api = ./arkive_api;
                     };
                   };
-                };
+                in
+                  pkgs.devshell.mkShell {
+                    packages = with pkgs; [ pyEnv poetry ];
 
-                config = lib.mkIf config.services.arkive-api.enable {
-                  users.users.arkive.isSystemUser = true;
-                  systemd.services.arkive-api = {
-                    after = [ "network.target" ];
-                    serviceConfig = {
-                      Type = "simple";
-                      User = "arkive";
-                      Group = "arkive";
-                      ExecStart = "${cfg.package}/bin/prod";
-                    };
+                    commands = [
+                      { package = pkgs.poetry; }
+                      { package = pkgs.pytest; }
+                      { package = pkgs.nixpkgs-fmt; }
+                      { package = pkgs.black; }
+                    ];
+
                   };
-                };
-              };
 
             }
       )
